3d9f17276473868ef65d8205a3c6ec91e641e5d1
tanbham <tanmay.bhambure@gmail.com>
2020-01-26 23:53:19 +0530

    Assignment 1 added

--- oop/assignment1/src/assignment1/test.java
+++ oop/assignment1/src/assignment1/test.java
@@ -3,10 +3,10 @@ package assignment1;
 public class test {
 	public static void main(String args[]) {
 		String s = "http://www.swabhavtech.com?developer=pawan&role=software";
-	    String new_s1 = s.replaceAll("=",".");
-	    String new_s2 = new_s1.replaceAll("&",".");
-	    System.out.println(new_s2);
-	    String a[] = new_s2.split("\\.");
+	    s = s.replaceAll("=",".");
+	    s = s.replaceAll("&",".");
+	    System.out.println(s);
+	    String a[] = s.split("\\.");
 	   
 	   //testing
 	   //for(String i : a ){
