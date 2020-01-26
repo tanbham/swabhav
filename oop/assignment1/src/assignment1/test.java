9d325e5877f38581b1d61017cf1485d1b4dcf4a8
Tanmay Bhambure <38174419+tanbham@users.noreply.github.com>
2020-01-26 20:04:10 +0530

    Update test.java

--- oop/assignment1/src/assignment1/test.java
+++ oop/assignment1/src/assignment1/test.java
@@ -2,7 +2,7 @@ package assignment1;
 
 public class test {
 	public static void main(String args[]) {
-		String s = "http://www.swabhavtech.com?developer=pawan&role=software";
+	    String s = "http://www.swabhavtech.com?developer=pawan&role=software";
 	    String new_s1 = s.replaceAll("=",".");
 	    String new_s2 = new_s1.replaceAll("&",".");
 	    System.out.println(new_s2);
