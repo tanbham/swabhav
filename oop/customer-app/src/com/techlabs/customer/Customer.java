fd8806c4613978ea02154e25dfd922de6ef5376f
tanbham <tanmay.bhambure@gmail.com>
2020-03-28 15:31:49 +0530

    cutomer app changes

--- oop/customer-app/src/com/techlabs/customer/Customer.java
+++ oop/customer-app/src/com/techlabs/customer/Customer.java
@@ -3,18 +3,18 @@ package com.techlabs.customer;
 public class Customer {
 	private String name;
 	private int order;
-	private String id = "1001";
-	private static int count;
+	private String id = "c";
+	private static int nextId;
 	
 	static {
-		count = 0;
+		nextId = 1001;
 	}
 	
 	public Customer(String name , int order) {
 		this.name = name;
 		this.order = order;
-		id = String.valueOf(Integer.valueOf(id)+count);
-		count++;
+		id = id + nextId;
+		nextId++;
 	}
 	
 	public String getName() {
@@ -26,6 +26,6 @@ public class Customer {
 	}
 	
 	public String getId() {
-		return "c"+id;
+		return id;
 	}
 }
